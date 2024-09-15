230c0f936534b77adb68ff11e0631037e4db992d
neelareddy <neelareddy.i0204@gmail.com>
2024-09-15 10:34:57 +0530

    good effects

--- backend.sh
+++ backend.sh
@@ -36,8 +36,14 @@ VALIDATE $? "Enabling nodejs"
 dnf install nodejs -y &>>$LOGFILE
 VALIDATE $? "Installing nodejs"
 
-useradd expense &>>$LOGFILE
-VALIDATE $? "Creating expense user"
+id expense
+if [ $? -ne 0 ]
+then
+    useradd expense &>>$LOGFILE
+    VALIDATE $? "Creating expense user"
+else
+    echo -e "Expense user already created...$Y SKIPPING $N"
+fi
 
 # mkdir /app
 
