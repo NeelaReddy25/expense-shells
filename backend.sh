51d29b43c8e01e1e3ce04a4a37e54b476822fb74
neelareddy <neelareddy.i0204@gmail.com>
2024-09-15 10:29:24 +0530

    good effects

--- backend.sh
+++ backend.sh
@@ -1,1 +1,74 @@
+#!/bin/bash
+
+USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+VALIDATE(){
+    if [ $1 -ne 0]
+    then
+        echo -e "$2...$R FAILURE $N"
+        exit 1
+    else
+        echo -e "$2...$G SUCCESS $N"
+    fi
+}
+
+if [ $USERID -ne 0 ]
+then
+    echo "Please run this script with root access."
+    exit 1 # manually exit if error comes.
+else
+    echo "You are super user."
+fi
+
+dnf module disable nodejs -y &>>$LOGFILE
+VALIDATE $? "Disabling default nodejs"
+
+dnf module enable nodejs:20 -y &>>$LOGFILE
+VALIDATE $? "Enabling nodejs"
+
+dnf install nodejs -y &>>$LOGFILE
+VALIDATE $? "Installing nodejs"
+
+useradd expense &>>$LOGFILE
+VALIDATE $? "Creating expense user"
+
+# mkdir /app
+
+
+# curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip
+
+
+# cd /app
+
+
+# unzip /tmp/backend.zip
+
+
+# npm install
+
+
+# systemctl daemon-reload
+
+
+# systemctl start backend
+
+
+# systemctl enable backend
+
+
+# dnf install mysql -y
+
+
+# mysql -h db.neelareddy.store -uroot -p${mysql_root_password} < /app/schema/backend.sql
+
+
+# systemctl restart backend
+
 
