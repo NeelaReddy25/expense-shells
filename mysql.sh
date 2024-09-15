d43b944e344b5840cd9d1be2c5d0e967d378c11e
neelareddy <neelareddy.i0204@gmail.com>
2024-09-15 09:43:55 +0530

    good effects

--- mysql.sh
+++ mysql.sh
@@ -8,6 +8,8 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
+echo "Please enter DB password:"
+read -s mysql_root_password
 
 VALIDATE(){
     if [ $1 -ne 0 ]
@@ -39,10 +41,10 @@ VALIDATE $? "Starting MySQL"
 # mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOGFILE
 # VALIDATE $? "Setting the root password"
 
-mysql -h db.neelareddy.store -uroot -pExpenseApp@1 -e 'show databases;' &>>$LOGFILE
+mysql -h db.neelareddy.store -uroot -p${mysql_root_password} -e 'show databases;' &>>$LOGFILE
 if [ $? -ne 0 ]
 then
-    mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOGFILE
+    mysql_secure_installation --set-root-pass ${mysql_root_password} &>>$LOGFILE
     VALIDATE $? "Setting the root password"
 else
     echo -e "MySQL root password is already setup...$Y SKIPPING $N"
